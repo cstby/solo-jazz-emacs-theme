c6e3f909b6c116f6b657c3afc1515b872a54bd8b
Carl Steib <carl.steib@gmail.com>
2020-10-05 11:44:18 -0700

    Include url and version number

--- solo-jazz-theme.el
+++ solo-jazz-theme.el
@@ -3,6 +3,8 @@
 ;; Copyright (C) 2020, Carl Steib
 
 ;; Author: Carl Steib
+;; URL: https://github.com/cstby/solo-jazz-emacs-theme
+;; Version: 0.6.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
